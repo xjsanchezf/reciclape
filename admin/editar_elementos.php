5d4fad64304f845ff4b61f4e4da125732d16e94b
Xiomara Sánchez <xjsanchezf@gmail.com>
2019-10-22 10:21:15 -0500

    Cambios en 'admin' y 'editar_elementos.php'

--- admin/editar_elementos.php
+++ admin/editar_elementos.php
@@ -10,8 +10,9 @@
     <?php   
     require '../config.php';
     $consulta_categorias = "SELECT id,nombre,descripcion FROM categorias ORDER BY nombre";
-    $consulta_items = "SELECT i.id,nombre,descripcion,icono,categoria,precio FROM item 
-        INNER JOIN 
+    $consulta_items = "SELECT i.id, i.nombre, i.descripcion, i.icono, c.id, i.precio 
+        FROM item AS i
+        INNER JOIN categorias AS c ON i.categoria = c.id
         ORDER BY nombre";
     ?>
 
@@ -28,6 +29,7 @@
             <th>Ícono</th>
             <th>Categoría</th>
             <th>Precio</th>
+            <th> </th>
         </tr>
         <?php 
         if ( $resultado_items = $mysqli->query($consulta_items) ) {
@@ -36,8 +38,17 @@
                 echo '<td><input type="text" name="nombre-item" value="'.$item[1].'"></td>';
                 echo '<td><input type="text" name="descripcion-item" value="'.$item[2].'"></td>';
                 echo '<td><input type="file" accept="image/*" name="icono-item" value="'.$item[3].'"></td>';
-                // Select 'Categorías'
-                echo '<td><select></select></td>';
+                echo '<td><select>';
+                if ( $resultado_categorias = $mysqli->query($consulta_categorias) ) {
+                    while ( $categoria = $resultado_categorias->fetch_row() ) {
+                        if ( $item[4] <> $categoria[0] ) {
+                            echo '<option value="'.$categoria[0].'">'.$categoria[1].'</option>';
+                        } else {
+                            echo '<option value="'.$categoria[0].'" selected>'.$categoria[1].'</option>';
+                        };
+                    };
+                };
+                echo '</select></td>';
                 echo '<td><input type="number" step="0.01" name="precio-item" value="'.$item[5].'"></td>';
                 echo '</tr>';
             }
